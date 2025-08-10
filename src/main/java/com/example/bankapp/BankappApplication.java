8dd6988da51fe15b40c1715719c1a57637508927
Prateek Chauhan <60933063+Prateekchhn9@users.noreply.github.com>
2025-08-10 19:58:39 +0200

    Update BankappApplication.java

--- src/main/java/com/example/bankapp/BankappApplication.java
+++ src/main/java/com/example/bankapp/BankappApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BankappApplication {
-
+ 
 	public static void main(String[] args) {
 		SpringApplication.run(BankappApplication.class, args);
 	}
